929097548eacbefc8e5b1c661d444143e39e0f53
Dan Dean <dandean218@gmail.com>
2019-04-17 20:40:12 -0500

    third upload

---
+++ bamazon.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+	item_id INTEGER (10) NOT NULL,
+    product_name VARCHAR(100),
+    department_name VARCHAR(100),
+    price INTEGER (10),
+    stock_quantity DECIMAL(10,4) NULL
+    );
+    
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (1, "Tube Socks", "Apparel", 42, 1000),
+	(201, "Corn printed lampshade", "Homegoods", 97, 54),
+    (82, "Tweezers", "First Aid", 4.5, 77),
+    (5641, "Captain Marvel Stickers", "Toys", 1.99, 7011),
+    (3, "yo-yo", "Toys", 5.49, 10000),
+    (34, "Yo-Yo Ma CD", "Music", 9.99, 10),
+    (554, "Water cooler", "Office", 700, 340), 
+    (19, "Tuxedo T-shirt", "Apparel", 19.99, 82),
+    (90, "Box of Random Paper Clips", "Office", 16, 240),
+    (48120, "Rose colored glasses", "Apparel", 1.40, 4);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,44 @@
-//this is my code
+//set up js file
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var figlet = require('figlet');
+
+//connect to mySQL
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306, 
+    user: "root",
+    password: "",
+    database: "bamazon"
+});
+
+connection.connect(function(err){
+    if(err) throw(err);
+    bamazonStart();
+});
+
+function bamazonStart(){
+    connection.query('SELECT * FROM products', function(err, res){
+        if(err) throw(err);
+    figlet('Welcome to BAMAZON', function(err, data){
+        if(err) throw(err);
+        console.log(data);
+    });
+
+    // for(let i = 0; i < res.length; i++){
+        console.table(res)
+    // }
+    })
+}
+
+
+//set up JS file that logs data from table
+
+//use inquirer to prompt user with questions
+
+//once order is placed by user, send mysql request to see if fake store has product available
+
+//use JS file to communicate status of order
+
+//use mysql to update data based on what customer wanted
+
